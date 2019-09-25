c5ad94049e724eef3fb800c24f8d809d98560fd8
rjannah92 <jannahroudlotul92@gmail.com>
2019-09-25 16:17:01 +0200

    powertable

--- printtable.py
+++ printtable.py
@@ -3,3 +3,9 @@ def multtable(number, limit):
         print(i * number)
 
 multtable(7, 10)
+
+def powertable(number, limit):
+    for i in range(1, limit+1):
+        print(i ** number)
+
+powertable(3, 3)
